a1e02a8c8c67a818ecdb8ac379ab21e1d2191b88
Sajid <Sajidmonowar11@gmail.com>
2022-12-02 17:31:16 -0500

    Tiny bit more fixing

--- 2/1.js
+++ 2/1.js
@@ -7,14 +7,13 @@ rounds.pop()
 
 let totalPoints = 0;
 
-let RPSval = {"A": ["Y", "X"], "B": ["Z", "Y"], "C": ["X", "Z"]}
-let RPSpoints = {"X": 1, "Y": 2, "Z": 3}
+let RPSval = {"A": ["Y", "X", 1], "B": ["Z", "Y", 2], "C": ["X", "Z", 3]}
 
 for (let round of rounds) {
     let played = round[2]
     let win = (played === RPSval[round[0]][0]) ? 6 : 0
     let draw = (played === RPSval[round[0]][1]) ? 3 : 0
-    totalPoints += RPSpoints[played]
+    totalPoints += RPSval[played][2]
     totalPoints += win
     totalPoints += draw
 }
