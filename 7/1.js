bf8440674269131291c33112d0703b89daa54e0c
Sajid <Sajidmonowar11@gmail.com>
2022-12-08 18:37:03 -0500

    idk it's an update

--- 7/1.js
+++ 7/1.js
@@ -3,23 +3,55 @@ import fs from "fs";
 const rawFile = fs.readFileSync('./7/input.txt',{encoding:'utf8'}).trim();
 
 class TreeNode {
-    constructor(value, descendants = [], parent = null) {
+    constructor(value, descendants = [], parent = null, size = null) {
         this.value = value;
         this.descendants = Array.from(descendants);
         this.parent = parent;
+        this.size = size;
+        if (this.size) {
+            this.addSizetoParent(this.size)
+        }
     }
 
     insertChild(descendants) {
         this.descendants.push(descendants)
         descendants.parent = this;
     }
+    
+    addSizetoParent(size) {
+        if (this.parent) {
+            this.parent.size += size
+            this.parent.addSizetoParent(size)
+        }
+    }
 }
 
 export function solution() {
-    let ok = new TreeNode("ok")
-    let ok1 = new TreeNode("ok1", "a")
-    ok.insertChild(ok1);
-    console.log(ok)
+    let lines = rawFile.split(/\n/gm)
+    let currentDirectory;
+    for (let line of lines) {
+        let fileSizematch = line.match(/([0-9]+) ([a-zA-Z.]+)/);
+        if (line.startsWith('$ cd')) {
+            let directory = line.slice(5)
+            switch (directory) {
+                case "..":
+                    currentDirectory = currentDirectory.parent;
+                    console.log("..", currentDirectory.value)
+                    break;
+                default:
+                    let node = new TreeNode(directory);
+                    if (currentDirectory) {
+                        currentDirectory.insertChild(node)
+                    }
+                    currentDirectory = node;
+                    console.log(currentDirectory.value, (currentDirectory.parent) ? currentDirectory.parent.value : currentDirectory.parent);
+                    break;
+            }
+        } else if (fileSizematch) {
+            let file = new TreeNode(fileSizematch[2], undefined, currentDirectory, parseInt(fileSizematch[1]))
+            currentDirectory.insertChild(file)
+        }
+    }
 }
 
 solution();
