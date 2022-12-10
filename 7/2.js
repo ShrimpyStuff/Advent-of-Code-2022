940a71319e1074d9a8df51db8be4ab2a2a3dfab8
Sajid <Sajidmonowar11@gmail.com>
2022-12-10 16:11:29 -0500

    Once again day 2 is stolen from @ElCholoGamer

--- 7/2.js
+++ 7/2.js
@@ -8,21 +8,11 @@ class TreeNode {
         this.descendants = Array.from(descendants);
         this.parent = parent;
         this.size = size;
-        if (this.size) {
-            this.addSizetoParent(this.size)
-        }
     }
 
     insertChild(descendants) {
         this.descendants.push(descendants)
         descendants.parent = this;
-    }
-    
-    addSizetoParent(size) {
-        if (this.parent) {
-            this.parent.size += size
-            this.parent.addSizetoParent(size)
-        }
     }
 }
 
@@ -30,15 +20,13 @@ export function solution() {
     let lines = rawFile.split(/\n/gm)
     let sizes = 0;
     let currentDirectory;
+
     for (let line of lines) {
         let fileSizematch = line.match(/([0-9]+) ([a-zA-Z.]+)/);
         if (line.startsWith('$ cd')) {
             let directory = line.slice(5)
             switch (directory) {
                 case "..":
-                    if (currentDirectory.size <= 100000) {
-                        sizes += currentDirectory.size
-                    }
                     currentDirectory = currentDirectory.parent;
                     break;
                 default:
@@ -54,7 +42,26 @@ export function solution() {
             currentDirectory.insertChild(file)
         }
     }
-    return sizes;
+    while (currentDirectory.parent) {
+        currentDirectory = currentDirectory.parent
+    }
+
+    function dirSize(dir, everyDirFunc) {
+        if (dir.size) return dir.size;
+
+        let size = dir.descendants.map(x => dirSize(x, everyDirFunc)).reduce((a, b) => a + b)
+
+        everyDirFunc(size);
+        return size;
+    }
+
+    let dirs = [];
+    let currentSize = dirSize(currentDirectory, size => {dirs.push(size)})
+    dirs.sort((a, b) => (a-b));
+    const sizeNeeded = 30_000_000 - (70_000_000 - currentSize)
+
+    return dirs.find(dirSize => dirSize >= sizeNeeded);
+    
 }
 
 solution();
